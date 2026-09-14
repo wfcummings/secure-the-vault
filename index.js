5a4ba1cd14f1e6235eb28b238e4c803318906677
willcummings <will.cummings2@gmail.com>
2026-09-14 13:58:31 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+// number 1
+let code1;
+
+//number 2
+code1 = 7 + 3;
+
+//number 3
+const code2 = 90 / 2 - 5;
+
+//number 4
+const code3 = (15 % 5) + 39;
+
+//number 5
+const message = "The vault has been secured. The combination is:";
+
+//number 6
+const codeA = code1 + "-" + code2 + "-" + code3;
+
+//number 7
+const codeB = "${code1}-${code2}-${code3}";
+
+//number 8
+consolelog(message, codeA, codeB);
